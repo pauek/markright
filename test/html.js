cca690561bebc024f2337f819706fdbc4a6cfe82
Pau Fernández <pau.fernandez@upc.edu>
2020-04-05 22:18:47 +0200

    Quito el Writer de html.js

--- test/html.js
+++ test/html.js
@@ -1,38 +1,5 @@
-const { parseFile, print, createProcessor } = require("../markright");
+const { parseFile, createProcessor } = require("../markright");
 const mr = parseFile("html.mr");
-// print(mr);
-// console.dir(mr, { depth: null });
-
-class Writer {
-  constructor(wstream) {
-    this.wstream = wstream;
-    this.indentation = 0;
-    this.lineStart = true;
-  }
-  endl() {
-    this.wstream.write("\n");
-    this.lineStart = true;
-  }
-  indented(fn) {
-    this.indentation += 2;
-    fn();
-    this.indentation -= 2;
-  }
-  indent(n) {
-    this.indentation += n;
-  }
-  write(x) {
-    if (this.lineStart) {
-      this.lineStart = false;
-      this.wstream.write(" ".repeat(this.indentation));
-    }
-    this.wstream.write(x);
-  }
-  writeln(x) {
-    this.write(x);
-    this.endl();
-  }
-}
 
 const html = createProcessor();
 
