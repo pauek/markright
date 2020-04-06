d01cda5dad611b809ef3ed25fd1e2579df997ef5
Pau Fernández <pau.fernandez@upc.edu>
2020-04-06 23:35:12 +0200

    Rehago el procesador (way better!)

--- markright.js
+++ markright.js
@@ -227,9 +227,9 @@ const parseFile = (filename) => {
   return parse(lines);
 };
 
-// Logger
+// Printer
 
-class Logger {
+class Printer {
   constructor(wstream) {
     this.wstream = wstream;
     this.indentation = 0;
@@ -251,151 +251,8 @@ class Logger {
   }
 }
 
-// Processor
-
-class Processor {
-  constructor() {
-    const defaultFn = ({ content }) => [...content];
-    this.funcTable = [
-      { path: "<markright>", fn: defaultFn },
-      { path: "<paragraph>", fn: defaultFn },
-      { path: "<line>", fn: defaultFn },
-      { path: "<text>", fn: ({ content }) => content },
-    ];
-  }
-
-  _on(path, fn) {
-    this.funcTable.push({ path, fn });
-  }
-  resolve(path) {
-    const last = path[path.length - 1];
-    for (let i = this.funcTable.length - 1; i >= 0; i--) {
-      const { path: tablePath, fn } = this.funcTable[i];
-      if (tablePath === last || (tablePath === "*" && !rInternal.test(last))) {
-        return fn;
-      }
-    }
-    return null;
-  }
-
-  markright(fn) {
-    this._on("<markright>", fn);
-  }
-  paragraph(fn) {
-    this._on("<paragraph>", fn);
-  }
-  line(fn) {
-    this._on("<line>", fn);
-  }
-  text(fn) {
-    this._on("<text>", fn);
-  }
-
-  cmd(path, fn) {
-    this._on(path, fn);
-  }
-  cmdList(pathList, fn) {
-    pathList.forEach((path) => {
-      this._on(path, fn);
-    });
-  }
-
-  process(root) {
-    const _contentGenerator = (path, mr) =>
-      function* () {
-        if (Array.isArray(mr.content)) {
-          for (let child of mr.content) {
-            yield _walk(path, child);
-          }
-        } else {
-          yield _walk(path, mr.content);
-        }
-      };
-
-    const _resolve = (mr, parentPath, name, contentGenerator) => {
-      const path = [...parentPath, name];
-      const func = this.resolve(path);
-      if (func) {
-        return func({
-          path: path,
-          cmd: mr instanceof Command ? mr : null,
-          content: contentGenerator(path, mr)(),
-        });
-      }
-      throw new Error(`Func not found for ${path}!`);
-    };
-
-    const _walk = (path, mr) => {
-      if (mr === null || mr === undefined) {
-        return;
-      }
-      switch (mr.constructor) {
-        case Markright:
-          return _resolve(mr, path, "<markright>", _contentGenerator);
-        case Paragraph:
-          return _resolve(mr, path, "<paragraph>", _contentGenerator);
-        case Line:
-          return _resolve(mr, path, "<line>", _contentGenerator);
-        case String:
-          return _resolve(mr, path, "<text>", () => () => mr);
-        case Command:
-          return _resolve(mr, path, mr.name, _contentGenerator);
-        default:
-          throw new Error(`Unexpected ${mr.constructor.name} (${mr})`);
-      }
-    };
-
-    return _walk([], root);
-  }
-}
-
-const createProcessor = () => {
-  return new Processor();
-};
-
-const walk = (mr) => {
-  const _walk = (mr) => {
-    if (mr === null || mr === undefined) {
-      return;
-    }
-    switch (mr.constructor) {
-      case Markright: {
-        console.log("Markright", mr.content);
-        mr.content.forEach(_walk);
-        break;
-      }
-      case Line: {
-        console.log("Line", mr.content);
-        mr.content.forEach(_walk);
-        break;
-      }
-      case Paragraph: {
-        console.log("Paragraph", mr.content);
-        mr.content.forEach(_walk);
-        break;
-      }
-      case Command: {
-        const cmd = mr;
-        console.log(`Command[${cmd.type}]`, cmd.name, cmd.args);
-        if (cmd.content) {
-          _walk(cmd.content, true);
-        }
-        break;
-      }
-      case String: {
-        console.log("Text", mr);
-        break;
-      }
-      default:
-        throw new Error(`Unexpected ${mr.constructor.name} (${mr})`);
-    }
-  };
-
-  _walk(mr);
-};
-
 const print = (mr, writeStream = process.stdout) => {
-  const logger = new Logger(writeStream);
+  const P = new Printer(writeStream);
 
   const _print = (mr) => {
     if (mr === null || mr === undefined) {
@@ -404,7 +261,7 @@ const print = (mr, writeStream = process.stdout) => {
     switch (mr.constructor) {
       case Markright: {
         mr.content.forEach((block, i) => {
-          if (i > 0) logger.endl();
+          if (i > 0) P.endl();
           _print(block);
         });
         break;
@@ -416,38 +273,38 @@ const print = (mr, writeStream = process.stdout) => {
       case Paragraph: {
         mr.content.forEach((item) => {
           _print(item);
-          if (item instanceof Line) logger.endl();
+          if (item instanceof Line) P.endl();
         });
         break;
       }
       case Command: {
         const cmd = mr;
-        logger.write(`@${cmd.name}`);
+        P.write(`@${cmd.name}`);
         if (cmd.args) {
-          logger.write(`(${cmd.args.join(",")})`);
+          P.write(`(${cmd.args.join(",")})`);
         }
         switch (cmd.type) {
           case CommandType.INLINE: {
             if (cmd.content) {
-              logger.write(`${cmd.openDelim}`);
+              P.write(`${cmd.openDelim}`);
               _print(cmd.content, true);
-              logger.write(`${cmd.closeDelim}`);
+              P.write(`${cmd.closeDelim}`);
             }
             break;
           }
           case CommandType.INPARAGRAPH:
           case CommandType.BLOCK: {
-            logger.endl();
-            logger.indent(2);
+            P.endl();
+            P.indent(2);
             _print(cmd.content);
-            logger.indent(-2);
+            P.indent(-2);
             break;
           }
         }
         break;
       }
       case String: {
-        logger.write(mr);
+        P.write(mr);
         break;
       }
       default:
@@ -458,11 +315,99 @@ const print = (mr, writeStream = process.stdout) => {
   _print(mr);
 };
 
+// Processor
+
+const isInternal = (pathElem) => rInternal.test(pathElem);
+
+const parsePath = (str) => str.split("/");
+
+const pathMatch = (modelPath, path) => {
+  const _elemMatch = (modelElem, elem) => {
+    return (
+      (modelElem === "*" && !isInternal(elem)) ||
+      (modelElem === "<*>" && isInternal(elem)) ||
+      modelElem === elem
+    );
+  };
+  const minLength = Math.min(path.length, modelPath.length);
+  for (let i = 0; i < minLength; i++) {
+    const j = path.length - 1 - i;
+    const mj = modelPath.length - 1 - i;
+    if (!_elemMatch(modelPath[mj], path[j])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+class FuncMap {
+  constructor() {
+    this.table = [];
+  }
+  on(pathStr, func) {
+    this.table.push({ path: parsePath(pathStr), func });
+  }
+  get(path) {
+    for (let i = this.table.length - 1; i >= 0; i--) {
+      const { path: modelPath, func } = this.table[i];
+      if (pathMatch(modelPath, path)) {
+        return func;
+      }
+    }
+    return null;
+  }
+}
+
+const process = (root, funcMap) => {
+  const currPath = [];
+
+  const _containerDefault = (mr) => mr.content.map(_process);
+  const _textDefault = (mr) => mr;
+
+  const _processElem = (mr, name, defaultFunc) => {
+    currPath.push(name);
+    const processFunc = funcMap.get(currPath) || defaultFunc;
+    const result = processFunc(mr, _process, currPath);
+    currPath.pop();
+    return result;
+  };
+
+  const _process = (mr) => {
+    if (mr === null || mr === undefined) {
+      return;
+    }
+    switch (mr.constructor) {
+      case Markright:
+        return _processElem(mr, "<markright>", _containerDefault);
+      case Paragraph:
+        return _processElem(mr, "<paragraph>", _containerDefault);
+      case Line:
+        return _processElem(mr, "<line>", _containerDefault);
+      case String:
+        return _processElem(mr, "<text>", _textDefault);
+      case Command: {
+        currPath.push(mr.name);
+        const processFunc = funcMap.get(currPath);
+        if (processFunc == null) {
+          throw new Error(`Cannot resolve "${currPath}"`);
+        }
+        const result = processFunc(mr, _process, currPath);
+        currPath.pop();
+        return result;
+      }
+      default:
+        throw new Error(`Unexpected ${mr.constructor.name} (${mr})`);
+    }
+  };
+
+  _process(root);
+};
+
 module.exports = {
   parse,
   parseLine,
   parseFile,
   print,
-  walk,
-  createProcessor,
+  FuncMap,
+  process,
 };
--- test/html-writer.js
+++ test/html-writer.js
@@ -1,29 +1,34 @@
-const { parseFile, createProcessor } = require("../markright");
+const mr = require("../markright");
 const Writer = require("./writer");
 
-const mr = parseFile("html.mr");
+const root = mr.parseFile("html.mr");
 const out = new Writer(process.stdout);
-const html = createProcessor();
+const html = new mr.FuncMap();
 
-html.text(({ content }) => out.write(content));
+html.on("<text>", (text) => out.write(text));
 
-html.paragraph(({ content }) => {
-  for (let _ of content) out.endl();
+html.on("<paragraph>", (paragraph, process) => {
+  paragraph.content.forEach((node) => {
+    process(node);
+    out.endl();
+  });
 });
 
-const tag = ({ cmd: { name, args, type }, content }) => {
+const processTag = (cmd, process) => {
+  const { name, args, type } = cmd;
   if (type === "block") {
     out.writeln(`<${name}${args ? " " + args.join(" ") : ""}>`);
-    out.indented(() => content.next());
+    out.indented(() => process(cmd.content));
     out.writeln(`</${name}>`);
   } else {
     out.write(`<${name}${args ? " " + args.join(" ") : ""}>`);
-    content.next();
+    process(cmd.content);
     out.write(`</${name}>`);
   }
 };
 
-html.cmd("*", tag);
-html.cmd("", () => out.write("<br>"));
+html.on("*", processTag);
 
-html.process(mr);
+html.on("", () => out.write("<br>"));
+
+mr.process(root, html);
